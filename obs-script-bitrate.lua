61d9a21676426a9f6b47c5c20546c35dcde04e98
Justin Love <git@JustinLove.name>
2018-02-19 10:47:34 -0600

    basic standard properties interface

--- obs-script-bitrate.lua
+++ obs-script-bitrate.lua
@@ -13,10 +13,27 @@ video_t *obs_get_video(void);
 
 local obsffi = ffi.load("obs")
 
-local width = 100
-local height = 100
+local width = 640
+local height = 360
 local bitrate = 1000
 local fps = 30
+
+local resolution_options = {
+	{640, 360},
+	{969, 392},
+	{768, 432},
+	{852, 480},
+	{960, 540},
+	{1096, 616},
+	{1152, 648},
+	{1280, 720},
+	{1140, 810},
+	{1536, 864},
+	{1600, 900},
+	{1920, 1080}
+}
+
+local fps_options = {10, 15, 20, 30}
 
 function dump_obs()
 	local keys = {}
@@ -69,6 +86,51 @@ function script_description()
 	return "Calculate bitrate tradeoffs, and maybe someday update settings in once place"
 end
 
+function script_properties()
+	obs.script_log(1, "props")
+
+	local props = obs.obs_properties_create()
+
+	local r = obs.obs_properties_add_list(props, "height", "Resolution", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_INT)
+	for _, res in ipairs(resolution_options) do
+		obs.obs_property_list_add_int(r, res[1] .. "x" .. res[2], res[2])
+	end
+
+	local f = obs.obs_properties_add_list(props, "fps", "FPS", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_INT)
+	for _, frames in ipairs(fps_options) do
+		obs.obs_property_list_add_int(f, tostring(frames), frames)
+	end
+
+	obs.obs_properties_add_int(props, "bitrate", "Bitrate", 1, 6000, bitrate)
+
+	return props
+end
+
+function script_defaults(settings)
+	obs.script_log(1, "defaults")
+
+	obs.obs_data_set_default_int(settings, "height", height)
+	obs.obs_data_set_default_int(settings, "fps", fps)
+	obs.obs_data_set_default_int(settings, "bitrate", bitrate)
+end
+
+function script_update(settings)
+	height = obs.obs_data_get_int(settings, "height")
+	for _, res in ipairs(resolution_options) do
+		if res[2] == height then
+			width = res[1]
+			break
+		end
+	end
+	fps = obs.obs_data_get_int(settings, "fps")
+	bitrate = obs.obs_data_get_int(settings, "bitrate")
+	obs.script_log(1, width .. "x" .. height .. " " .. fps .. "fps @ " .. bitrate)
+end
+
+function script_save(settings)
+	obs.script_log(1, "save")
+end
+
 function script_load(settings)
 	--dump_obs()
 	local encoder = obs.obs_get_encoder_by_name("simple_h264_stream")
@@ -92,4 +154,7 @@ function script_load(settings)
 	end
 
 	obs.script_log(1, width .. "x" .. height .. " " .. fps .. "fps @ " .. bitrate)
+	obs.obs_data_set_int(settings, "height", height)
+	obs.obs_data_set_int(settings, "fps", fps)
+	obs.obs_data_set_int(settings, "bitrate", bitrate)
 end
